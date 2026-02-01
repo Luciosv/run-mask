f06f7ef0c6637f074606c66f968fd061e558bc2a
Lucio Sepulveda <lucio.sepulveda.12@gmail.com>
2026-01-31 23:29:10 -0300

    enemigo azul terminado

--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -6,6 +6,7 @@ var can_move := false
 
 func _ready() -> void:
 	Mask.changed_mask.connect(_on_changed_mask)
+	can_move = not Mask.current_mask == color
 
 
 func _on_changed_mask(mask:MaskManager.ColorMask):
